aa94589bedaee5f932623f61353ad1f4703ed9f2
Lokidottir <ma302fh@gold.ac.uk>
2016-06-12 13:10:25 +0100

    Renamed parameterized type of SimplyTyped to avoid confusion

--- src/Calculi/Lambda/Cube/Systems/SimplyTyped.hs
+++ src/Calculi/Lambda/Cube/Systems/SimplyTyped.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Calculi.Lambda.Cube.Systems.SimplyTyped where
 
 import           Calculi.Lambda
@@ -18,51 +19,49 @@ import qualified Language.Haskell.TH.Lift as TH
 {-|
     Data type describing a type system for simply-typed lambda calculus (λ→).
 -}
-data SimplyTyped t =
-      Mono t
-    | Function (SimplyTyped t) (SimplyTyped t)
+data SimplyTyped m =
+      Mono m
+    | Function (SimplyTyped m) (SimplyTyped m)
     deriving (Show, Read, Eq, Ord, Data)
 
 TH.deriveLift ''SimplyTyped
 
 instance Functor SimplyTyped where
     fmap f = \case
-        Mono t -> Mono (f t)
-        Function t1 t2 -> Function (fmap f t1) (fmap f t2)
+        Mono m -> Mono (f m)
+        Function from to -> Function (fmap f from) (fmap f to)
 
 instance Foldable SimplyTyped where
     foldr f z = \case
-        Mono t -> f t z
-        Function t1 t2 -> foldr f (foldr f z t2) t1
+        Mono m -> f m z
+        Function from to -> foldr f (foldr f z to) from
 
-instance Ord t => SimpleType (SimplyTyped t) where
-    type MonoType (SimplyTyped t) = t
+instance Ord m => SimpleType (SimplyTyped m) where
+    type MonoType (SimplyTyped m) = m
 
     abstract = Function
 
-    reify (Function x y) = Just (x, y)
-    reify _              = Nothing
+    reify (Function from to) = Just (from, to)
+    reify _                  = Nothing
 
-    bases (Mono t)         = Set.singleton (Mono t)
-    bases (Function t1 t2) = bases t1 `Set.union` bases t2
+    bases (Mono m)           = Set.singleton (Mono m)
+    bases (Function from to) = bases from `Set.union` bases to
 
     mono = Mono
 
-instance (Ord v, Ord t) => Typecheckable v (SimplyTyped t) where
-    type TypingContext v (SimplyTyped t) = (TypingEnvironment v (SimplyTyped t))
+instance (Ord v, Ord m) => Typecheckable v (SimplyTyped m) where
+    type TypingContext v (SimplyTyped m) = (SimpleTypingContext v (SimplyTyped m))
 
-    -- Horrible error wrapper that gives a lot of information.
-    type TypeError v (SimplyTyped t) =
-        ErrorContext
-            TypingEnvironment
+    type TypeError v (SimplyTyped m) =
+        -- Using an ErrorContext because it can hold a lot of information.
+        ErrorContext'
             v
-            (SimplyTyped t)
-            (NotKnownErr v (SimplyTyped t) :+: SimpleTypeErr (SimplyTyped t))
+            (SimplyTyped m) (NotKnownErr v (SimplyTyped m) :+: SimpleTypeErr (SimplyTyped m))
 
     typecheck env _expr =
-        -- Always cons the current expression to all the errors that have occurred
+        -- Always add the current expression to all the errors that have occurred
         -- so there's a list of expressions towards the error that happened.
-        first (fmap (\err -> err { expression = _expr : expression err }))
+        first (fmap (\err -> err { _expression = _expr : _expression err }))
         $ case _expr of
             {-
                 Case of a LC variable, not really typechecking but instead
@@ -73,7 +72,7 @@ instance (Ord v, Ord t) => Typecheckable v (SimplyTyped t) where
                     err = Left [ErrorContext [] env (Left (UnknownVariable v))]
                     -- If it succeeds in it's lookup, pipe the result into a tuple in a Right value.
                     success = (Right . (,) env)
-                in maybe err success (Map.lookup v (vars env))
+                in maybe err success (Map.lookup v (_vars env))
 
             {-
                 Application of two expressions.
@@ -109,18 +108,18 @@ instance (Ord v, Ord t) => Typecheckable v (SimplyTyped t) where
                 -- Get any types in var'type not in the set of all types
                 -- as every type should be declared by the environment
                 -- before appearing in an expression.
-                let unknownTypes = Set.toList $ Set.difference (bases var'type) (allTypes env)
+                let unknownTypes = Set.toList $ Set.difference (bases var'type) (_allTypes env)
                 let errFun t = ErrorContext [] env (Left $ UnknownType t)
                 -- If there are unknown types, error on them.
                 unless (null unknownTypes) . Left $ errFun <$> unknownTypes
                 -- If we get ths far, then var'type was valid
                 -- Set a new typing context with var brought into scope
-                let env' = env { vars = Map.insert var var'type (vars env) }
+                let env' = env { _vars = Map.insert var var'type (_vars env) }
                 -- Typecheck the lambda's body
                 (_, expr'type) <- typecheck env' expr
                 -- Return the environment and the type of the lambda expression.
                 return (env, var'type /-> expr'type)
 
-instance (Data t, Arbitrary t) => Arbitrary (SimplyTyped t) where
+instance (Data m, Arbitrary m) => Arbitrary (SimplyTyped m) where
     -- TODO: remove instance of Data for t
     arbitrary = sized generatorP
