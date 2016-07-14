ec307e018c27b985e2c36617c7da36e4a04f8982
Lokidottir <ma302fh@gold.ac.uk>
2016-07-14 12:05:04 +0100

    Began moving to a lambda term AST that has parameterized constants

--- src/Calculi/Lambda.hs
+++ src/Calculi/Lambda.hs
@@ -1,6 +1,7 @@
 module Calculi.Lambda (
       -- * Typed Lambda Calculus AST.
       LambdaExpr(..)
+    , LambdaTerm(..)
     , UntypedLambdaExpr
       -- ** Analysis Helpers
     , freeVars
@@ -33,15 +34,6 @@ data LambdaExpr v t =
     | Lambda (v, t) (LambdaExpr v t)          -- ^ A variable definition and a function body
     deriving (Eq, Ord, Show, Data)
 
-{- Sketching out possible changes to LambdaExpr -}
-data LambdaTerm c v t =
-      Variable v
-    | Constant c
-    | Apply_ (LambdaTerm c v t) (LambdaTerm c v t)
-    | Lambda_ (v, t) (LambdaTerm c v t) (LambdaTerm c v t)
-
-type LetDeclr v t = ((v, t), LambdaExpr v t)
-
 deriveBifunctor ''LambdaExpr
 deriveBifoldable ''LambdaExpr
 deriveBitraversable ''LambdaExpr
@@ -53,75 +45,103 @@ instance (Arbitrary v, Data v, Arbitrary t, Data t) => Arbitrary (LambdaExpr v t
 type UntypedLambdaExpr v = LambdaExpr v ()
 
 {-|
+    A simple, typed lambda calculus AST with constants.
+-}
+data LambdaTerm c v t =
+      Variable v
+      -- ^ A reference to a variable.
+    | Constant c
+      -- ^ A constant value, such as @""@.
+    | Apply_ (LambdaTerm c v t) (LambdaTerm c v t)
+      -- ^ An application of one expression to another.
+    | Lambda_ (v, t) (LambdaTerm c v t)
+      -- ^ A lambda expression, with a variable definition and
+      -- a function body.
+    deriving (Eq, Ord, Show, Data)
+
+deriveBifunctor ''LambdaTerm
+deriveBifoldable ''LambdaTerm
+deriveBitraversable ''LambdaTerm
+
+instance (Arbitrary c, Data c,
+          Arbitrary v, Data v,
+          Arbitrary t, Data t) => Arbitrary (LambdaTerm c v t) where
+    arbitrary = sized generatorSR
+
+type LetDeclr c v t = ((v, t), LambdaTerm c v t)
+
+{-|
     Given the contents of a let expression's declarations, generate a graph
     showing the dependencies between each declaration, including recursion
     as loops.
 -}
-letsDependency :: (DynGraph gr, Ord v, Ord t) => [LetDeclr v t] -> gr (LetDeclr v t) ()
+letsDependency :: (DynGraph gr, Ord c, Ord v, Ord t) => [LetDeclr c v t] -> gr (LetDeclr c v t) ()
 letsDependency = snd . letsDependency'
 
 {-|
     `letsDependency` but also return the internally used NodeMap.
 -}
-letsDependency' :: forall v t gr. (DynGraph gr, Ord v, Ord t) => [LetDeclr v t] -> (NodeMap (LetDeclr v t), gr (LetDeclr v t) ())
+letsDependency' :: forall gr c v t. (DynGraph gr, Ord c, Ord v, Ord t)
+                => [LetDeclr c v t]
+                -> (NodeMap (LetDeclr c v t), gr (LetDeclr c v t) ())
 letsDependency' lets =
     let
         declrs = fst . fst <$> lets
         declrsSet = Set.fromList declrs
         -- memoize because we need to check the vars then use the entire declaration and this
         -- is very costly.
-        declrMap :: Map.Map v (LetDeclr v t)
+        declrMap :: Map.Map v (LetDeclr c v t)
         declrMap = Map.fromList $ zip declrs lets
 
         {-
             Get the intersection of the free variables in the expression and the set of
             declarations in this let expression.
         -}
-        dependsOf :: LetDeclr v t -> Set.Set v
+        dependsOf :: LetDeclr c v t -> Set.Set v
         dependsOf = Set.intersection declrsSet . freeVars . snd
 
         {-
             Find all the inward nodes of a declaration.
         -}
-        inwardNodes :: LetDeclr v t -> [LetDeclr v t]
+        inwardNodes :: LetDeclr c v t -> [LetDeclr c v t]
         inwardNodes declr =
             fromMaybe [] (sequence $ (`Map.lookup` declrMap) <$> Set.toList (dependsOf declr))
 
-        inwardEdges :: LetDeclr v t -> [(LetDeclr v t, LetDeclr v t, ())]
+        inwardEdges :: LetDeclr c v t -> [(LetDeclr c v t, LetDeclr c v t, ())]
         inwardEdges declr = (\inward -> (inward, declr, ())) <$> inwardNodes declr
     in snd . run empty $ insMapEdgesM (concat $ inwardEdges <$> lets)
 
 {-|
     Unlet non-cyclic let expressions.
 -}
-unlet :: forall v t.
-       (Ord v, Ord t)
-    => [LetDeclr v t] -- ^ The list of declarations in a let expression
-    -> LambdaExpr v t -- ^ The body of the let declaration
-    -> Either [[LetDeclr v t]]
-              (LambdaExpr v t) -- ^ Either a list of cyclic lets or the final expression
+unlet :: forall c v t. (Ord c, Ord v, Ord t)
+      => [LetDeclr c v t] -- ^ The list of declarations in a let expression
+      -> LambdaTerm c v t -- ^ The body of the let declaration
+      -> Either [[LetDeclr c v t]]
+                (LambdaTerm c v t) -- ^ Either a list of cyclic lets or the final expression
 unlet lets expr =
     let -- Build the dependency graph
-        depends :: Gr (LetDeclr v t) () = letsDependency lets
+        depends :: Gr (LetDeclr c v t) () = letsDependency lets
         -- Get the regular topsort.
         tsorted            = topsortWithCycles depends
         (cycles, lets')    = partitionEithers tsorted
         -- This is what turns the cycles found in tsorted
         -- to let expressions and the non-cycle nodes into
         -- lambda-apply name scoping.
-        unlet' (declr, body) lexpr = Lambda declr lexpr `Apply` body
+        unlet' (declr, body) lexpr = Lambda_ declr lexpr `Apply_` body
     in if null cycles then Right (foldr unlet' expr lets') else Left cycles
 
 {-|
     Find all the unbound variables in an expression.
 -}
-freeVars :: Ord v => LambdaExpr v t -> Set.Set v
+freeVars :: Ord v => LambdaTerm c v t -> Set.Set v
 freeVars = \case
-    Var v              -> Set.singleton v
-    Apply fun arg      ->
+    Variable v          -> Set.singleton v
+    Constant _          -> Set.empty
+    Apply_ fun arg      ->
         -- Union the free variables of both parts of the Apply
         freeVars fun <> freeVars arg
-    Lambda (v, _) expr ->
+    Lambda_ (v, _) expr ->
         -- remove the variable defined by the lambda from the set of free
         -- variables found in the body.
         Set.delete v (freeVars expr)
--- src/Calculi/Lambda/Cube/Dependent.hs
+++ src/Calculi/Lambda/Cube/Dependent.hs
@@ -4,14 +4,22 @@ import           Calculi.Lambda
 import           Calculi.Lambda.Cube.SimpleType
 
 {-|
-    Typesystems which can
+    Typesystems which can have values at the type-level.
 -}
 class SimpleType t => Dependent t where
 
-    -- | The variable type to be used
-    type VarType t :: *
+    {-|
+        The type representing value-level variables at the type level.
+    -}
+    type DependentVariable t :: *
 
     {-|
-        Lift a value to a type.
+        The type representing value-level constants at the type level.
     -}
-    valueToType :: LambdaExpr (VarType t) t -> t
+    type DependentConstant t :: *
+
+    {-|
+        Encode a value at the type-level. This could be just a constructor or
+        a complete transformation of the AST, but this typeclass doesn't care.
+    -}
+    valueToType :: LambdaTerm (DependentConstant t) (DependentVariable t) t -> t
