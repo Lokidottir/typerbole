a3655ac0f2bf85ce4ab185f398a59a8e16254fc9
Lokidottir <ma302fh@gold.ac.uk>
2016-08-11 18:30:43 +0100

    Dependent now uses a multiparameter typeclass instead of associated types

--- src/Calculi/Lambda/Cube/Dependent.hs
+++ src/Calculi/Lambda/Cube/Dependent.hs
@@ -3,19 +3,12 @@ module Calculi.Lambda.Cube.Dependent where
 import Calculi.Lambda.Cube.SimpleType
 
 {-|
-    Typesystems which can have values in their types.
+    Typesystems which can have values (@term@) in their types (@t@).
 -}
-class SimpleType t => Dependent t where
-    
-    {-|
-        A value-level term that can be encoded as a type expression.
-
-        Of kind @* -> *@ because it expects a typesystem as a parameter.
-    -}
-    type DependentTerm t :: * -> *
+class SimpleType t => Dependent term t where
 
     {-|
         Encode a value at the type-level. This could be just a constructor or
         a complete transformation of the AST, but this typeclass doesn't care.
     -}
-    valueToType :: (DependentTerm t) t -> t
+    valueToType :: term t -> t
