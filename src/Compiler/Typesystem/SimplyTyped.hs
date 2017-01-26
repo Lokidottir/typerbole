007a0f4ecaeef3d30fcc20328fbfe54be3208c7b
Lokidottir <ma302fh@gold.ac.uk>
2017-01-26 20:26:43 +0000

    Added implementation for PureTypecheckable on SimplyTyped

--- src/Compiler/Typesystem/SimplyTyped.hs
+++ src/Compiler/Typesystem/SimplyTyped.hs
@@ -1,12 +1,16 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Compiler.Typesystem.SimplyTyped where
 
 import           Calculi.Lambda
 import           Calculi.Lambda.Cube.SimpleType
-import           Control.Typecheckable
+import           Control.Typecheckable.PureTypecheckable
 import           Control.Monad
+import           Control.Lens
 import qualified Control.Monad.State            as State
+import qualified Control.Monad.Except           as Except
+import qualified Data.List.NonEmpty as NE
 import           Data.Bifunctor
 import           Data.Generics
 import qualified Data.Map                       as Map
@@ -15,25 +19,26 @@ import           Data.Semigroup
 import qualified Data.Set                       as Set
 import           Test.QuickCheck
 import qualified Language.Haskell.TH.Lift as TH
+import           Data.Either.Validation
 
 {-|
     Data type describing a type system for simply-typed lambda calculus (λ→).
 -}
-data SimplyTyped m =
-      Mono m
-    | Function (SimplyTyped m) (SimplyTyped m)
+data SimplyTyped c =
+      TypeCon c
+    | Function (SimplyTyped c) (SimplyTyped c)
     deriving (Show, Read, Eq, Ord, Data)
 
 TH.deriveLift ''SimplyTyped
 
 instance Functor SimplyTyped where
     fmap f = \case
-        Mono m -> Mono (f m)
+        TypeCon c -> TypeCon (f c)
         Function from to -> Function (fmap f from) (fmap f to)
 
 instance Foldable SimplyTyped where
     foldr f z = \case
-        Mono m -> f m z
+        TypeCon c -> f c z
         Function from to -> foldr f (foldr f z to) from
 
 instance Ord m => SimpleType (SimplyTyped m) where
@@ -41,7 +46,7 @@ instance Ord m => SimpleType (SimplyTyped m) where
 
     abstract = Function
 
-    typeconst = Mono
+    typeconst = TypeCon
 
     equivalent = (==)
 
@@ -50,84 +55,78 @@ instance Ord m => SimplyTypedUtil (SimplyTyped m) where
     unabstract (Function from to) = Just (from, to)
     unabstract _                  = Nothing
 
-    bases (Mono m)           = Set.singleton (Mono m)
+    bases (TypeCon c)           = Set.singleton (TypeCon c)
     bases (Function from to) = bases from `Set.union` bases to
 
 data SimplyTypedErr c v t =
       STNotKnownErr (NotKnownErr c v t)
     | STSimpleTypeErr (SimpleTypeErr t)
     deriving (Eq, Ord, Show)
-{-
-instance (Ord m, Ord c, Ord v) => Typecheckable (LambdaTerm c v) (SimplyTyped m) where
-    type TypingContext (LambdaTerm c v) (SimplyTyped m) = (SimpleTypingContext c v (SimplyTyped m))
 
-    type TypeError (LambdaTerm c v) (SimplyTyped m) =
-        -- Using an ErrorContext because it can hold a lot of information.
-        ErrorContext'
-            (LambdaTerm c v)
-            (SimplyTyped m) (SimplyTypedErr c v (SimplyTyped m))
+instance (Ord termcon, Ord var, Ord typecon) => PureTypecheckable (LambdaTerm termcon var) (SimplyTyped typecon) where
 
-    typecheck env _expr =
-        -- Always add the current expression to all the errors that have occurred
-        -- so there's a list of expressions towards the error that happened.
-        first (fmap (\err -> err { _expression = _expr : _expression err }))
-        $ case _expr of
-            {-
-                Case of a LC variable, not really typechecking but instead
-                checking that the variable name is in scope.
-            -}
-            Variable v ->
-                let -- If there's an error on the var lookup, return this as the error.
-                    err = Left [ErrorContext [] env (STNotKnownErr (UnknownVariable v))]
-                    -- If it succeeds in it's lookup, pipe the result into a tuple in a Right value.
-                    success = (Right . (,) env)
-                in maybe err success (Map.lookup v (_variables env))
+    type TypeError (LambdaTerm termcon var) (SimplyTyped typecon)
+        = [SimplyTypedErr termcon var (SimplyTyped typecon)]
 
-            {-
-                Application of two expressions.
-            -}
-            (Apply fun arg) ->
-                let -- Typecheck both the function side and the argument
-                    fun' = typecheck env fun
-                    arg' = typecheck env arg
-                in case (fun', arg') of
-                    -- If both errored, then combine the errors.
-                    (Left fun'err, Left arg'err) -> Left $ fun'err <> arg'err
-                    _ -> do
-                        -- discard their contexts
-                        (_, fun'type) <- fun'
-                        (_, arg'type) <- arg'
-                        case unabstract fun'type of
-                            Nothing       ->
-                                -- The first expression's type wasn't a function type.
-                                Left [ErrorContext [fun] env (STSimpleTypeErr $ NotAFunction fun'type)]
-                            Just (funarg'type, funret'type)
-                                | funarg'type /= arg'type ->
-                                    -- The type of the argument expression doesn't match the expected
-                                    -- type of the function expression.
-                                    Left [ErrorContext [fun] env (STSimpleTypeErr $ UnexpectedType funarg'type arg'type)]
-                                | otherwise ->
-                                    -- succeeded in typechecking.
-                                    Right (env, funret'type)
+    type TypingContext (LambdaTerm termcon var) (SimplyTyped typecon)
+        = SimpleTypingContext termcon var (SimplyTyped typecon)
 
-            {-
-                Abstracting a variable over an expression.
-            -}
-            (Lambda (var, var'type) expr) -> do
-                -- Get any types in var'type not in the set of all types
-                -- as every type should be declared by the environment
-                -- before appearing in an expression.
-                let unknownTypes = Set.toList $ Set.difference (bases var'type) (_allTypes env)
-                let errFun t = ErrorContext [] env (STNotKnownErr$ UnknownType t)
-                -- If there are unknown types, error on them.
-                unless (null unknownTypes) . Left $ errFun <$> unknownTypes
-                -- If we get ths far, then var'type was valid
-                -- Set a new typing context with var brought into scope
-                let env' = env { _variables = Map.insert var var'type (_variables env) }
-                -- Typecheck the lambda's body
-                (_, expr'type) <- typecheck env' expr
-                -- Return the environment and the type of the lambda expression.
-                return (env, var'type /-> expr'type)
--}
+    typecheckP term ctx = Except.runExcept $ State.runStateT (typecheckP_ term) ctx where
+        typecheckP_ :: forall t term tyctx tyerr m.
+                    ( t ~ (SimplyTyped typecon)
+                    , term ~ (LambdaTerm termcon var)
+                    , tyctx ~ TypingContext term t
+                    , tyerr ~ TypeError term t
+                    , m ~ State.StateT tyctx (Except.Except tyerr)
+                    )
+                    => term t
+                    -> m t
+        typecheckP_ = typecheckP_final where
+
+            -- | lookup the type of a variable, or report an error
+            varlookup :: var -> m t
+            varlookup var = State.gets (Map.lookup var . _variables) >>= \case
+                Nothing -> Except.throwError $ pure (STNotKnownErr (UnknownVariable var))
+                Just t -> return t
+
+            -- | lookup the type of a constant, or report an error
+            constlookup :: termcon -> m t
+            constlookup con = State.gets (Map.lookup con . _constants) >>= \case
+                Nothing -> Except.throwError $ pure (STNotKnownErr (UnknownConstant con))
+                Just t -> return t
+
+            -- | get the type of the application of one term to another.
+            termAp :: term t -> term t -> m t
+            termAp f x = join $ deriveType <$> typecheckP_ f <*> typecheckP_ x where
+                -- | derive the type of applying terms with the given types.
+                deriveType :: t -> t -> m t
+                deriveType f't x't = case f't of
+                    a `Function` b
+                        -- if a is the same as the type of x's type, then return b.
+                        | a `equivalent` x't -> return b
+                        | otherwise ->
+                            Except.throwError $ pure (STSimpleTypeErr (UnexpectedType a x't))
+                    -- 'f' is not a function
+                    _ -> Except.throwError $ pure (STSimpleTypeErr (NotAFunction f't))
+
+            lambda :: (var, t) -> term t -> m t
+            lambda (v, t) term =
+                withTempState (variables %~ Map.insert v t) $ (t /->) <$> typecheckP_ term where
+                    -- Perform an action with a modified state, returning the
+                    -- state to it's orginal form once the action is complete.
+                    withTempState :: (tyctx -> tyctx) -> m a -> m a
+                    withTempState f action = do
+                        state <- State.get
+                        State.modify f
+                        result <- action
+                        State.put state
+                        return result
+
+            typecheckP_final = \case
+                Variable v -> varlookup v
+                Constant c -> constlookup c
+                Apply f x -> termAp f x
+                Lambda declr term -> lambda declr term
+
 instance (Data m, Arbitrary m) => Arbitrary (SimplyTyped m) where
     arbitrary = sized $ generatorPWith [alias (\() -> arbitrary :: Gen m)]
